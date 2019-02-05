135fe5472719ffe1e148047eb2c536abd5fc6306
khadidja <khadidja.adam2014@gmail.com>
2019-02-05 15:20:19 -0500

    Commit 3. Add logout test

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -15,6 +15,7 @@ import java.util.concurrent.TimeUnit;
 
 public class LoginTests {
     WebDriver driver;
+
     @BeforeMethod
     public void setUp() {//if you stock in Terminal just click esc+q and then on  wq at at the same time and you will go back
         WebDriverManager.chromedriver().setup();
@@ -30,6 +31,18 @@ public class LoginTests {
         driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test" + Keys.ENTER);
         Assert.assertEquals(driver.getTitle(), "Web Orders");
     }
+
+
+    @Test
+    public void LogOutTest() {
+        driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
+        driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
+        driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test" + Keys.ENTER);
+        driver.findElement(By.id("ctl00_logout")).click();
+        Assert.assertEquals(driver.getTitle(), "Web Orders Login");
+
+    }
+
     @AfterMethod
     public void CleanUp() {
         driver.close();
